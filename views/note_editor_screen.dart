3ec3572988e1dd7ee039e15820e4e9f410977a7d
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-13 22:15:26 +0100

    Implement NoteEditorScreen with text fields

--- views/note_editor_screen.dart
+++ views/note_editor_screen.dart
@@ -1,1 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/note_model.dart';
+import '../providers/app_providers.dart';
 
+class NoteEditorScreen extends ConsumerStatefulWidget {
+  final Note note;
+  const NoteEditorScreen({super.key, required this.note});
+
+  @override
+  ConsumerState<NoteEditorScreen> createState() => _NoteEditorScreenState();
+}
+
+class _NoteEditorScreenState extends ConsumerState<NoteEditorScreen> {
+  late TextEditingController _titleController;
+  late TextEditingController _contentController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.note.title);
+    _contentController = TextEditingController(text: widget.note.content);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+
+  void _saveData() {
+    ref.read(notesProvider.notifier).updateNote(
+          widget.note.id,
+          _titleController.text,
+          _contentController.text,
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(elevation: 0),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              onChanged: (_) => _saveData(),
+              style: const TextStyle(fontSize: 26, fontWeight: FontWeight.black),
+              decoration: const InputDecoration(
+                hintText: 'Document Title',
+                border: InputBorder.none,
+              ),
+            ),
+            const Divider(),
+            Expanded(
+              child: TextField(
+                controller: _contentController,
+                onChanged: (_) => _saveData(),
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                style: const TextStyle(fontSize: 16, height: 1.6),
+                decoration: const InputDecoration(
+                  hintText: 'Start writing your masterpiece...',
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
