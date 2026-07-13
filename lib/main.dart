86a8f9a7694b9d49c0018a13df2f53ffc4a9807a
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-13 23:20:24 +0100

    Refactor MyApp and change home screen to SplashScreen
    Refactored MyApp to be stateless and changed the home screen to SplashScreen.

--- lib/main.dart
+++ lib/main.dart
@@ -5,42 +5,21 @@ import 'models/note_model.dart';
 import 'providers/app_providers.dart';
 import 'services/ad_service.dart';
 import 'theme/app_theme.dart';
-import 'views/home_screen.dart';
+import 'views/splash_screen.dart'; // NEW
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // 1. Init Hive DB
   await Hive.initFlutter();
   Hive.registerAdapter(NoteModelAdapter());
   Hive.registerAdapter(FolderModelAdapter());
   await Hive.openBox<NoteModel>('notes');
   await Hive.openBox<FolderModel>('folders');
-  
-  // 2. Init Ads
   await AdService.init();
-  
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    // 3. Show Launch Interstitial 2 seconds after app starts
-    Future.delayed(const Duration(seconds: 2), () {
-      if(mounted) {
-        AdService.showLaunchAd(onAdDismissed: () {});
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +28,7 @@ class _MyAppState extends State<MyApp> {
       child: MaterialApp(
         title: 'WyNote',
         theme: AppTheme.lightTheme,
-        home: HomeScreen(),
+        home: const SplashScreen(), // CHANGED FROM HomeScreen()
         debugShowCheckedModeBanner: false,
       ),
     );
