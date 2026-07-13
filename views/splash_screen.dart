6b34f4c55a8d7d3b32300cf9796cfa731eadf8f9
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-13 22:08:46 +0100

    Update splash_screen.dart

--- views/splash_screen.dart
+++ views/splash_screen.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
+import '../services/ad_service.dart';
+import 'home_screen.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _handleLaunchSequence();
+  }
+
+  void _handleLaunchSequence() async {
+    // Synchronously wait for the AdMob system network hook up
+    await AdService.loadLaunchAd();
+    
+    AdService.showLaunchAd(
+      onAdDismissed: () {
+        if (mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const HomeScreen()),
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'WyNote',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.extrabold, letterSpacing: 1.2),
+            ),
+            SizedBox(height: 24),
+            CircularProgressIndicator.adaptive(),
+          ],
+        ),
+      ),
+    );
+  }
+}
