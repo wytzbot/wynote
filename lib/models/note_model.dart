b9304907085448de8736c9a69831e603e9c3e228
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-13 23:00:46 +0100

    Refactor Note to NoteModel and add FolderModel
    Refactor Note class to NoteModel with Hive support and add FolderModel.

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,27 +1,31 @@
-class Note {
-  final String id;
-  final String title;
-  final String content;
-  final DateTime updatedAt;
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
 
-  Note({
+@HiveType(typeId: 0)
+class NoteModel {
+  @HiveField(0) String id;
+  @HiveField(1) String title;
+  @HiveField(2) String content; // Quill Delta JSON
+  @HiveField(3) String folderId;
+  @HiveField(4) String color;
+  @HiveField(5) DateTime date;
+  @HiveField(6) bool isDeleted;
+
+  NoteModel({
     required this.id,
     required this.title,
     required this.content,
-    required this.updatedAt,
+    this.folderId = '',
+    this.color = '#FFF9C4',
+    required this.date,
+    this.isDeleted = false,
   });
+}
 
-  Note copyWith({
-    String? id,
-    String? title,
-    String? content,
-    DateTime? updatedAt,
-  }) {
-    return Note(
-      id: id ?? this.id,
-      title: title ?? this.title,
-      content: content ?? this.content,
-      updatedAt: updatedAt ?? this.updatedAt,
-    );
-  }
+@HiveType(typeId: 1)
+class FolderModel {
+  @HiveField(0) String id;
+  @HiveField(1) String name;
+
+  FolderModel({required this.id, required this.name});
 }
