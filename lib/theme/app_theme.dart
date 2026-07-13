3576de08d121caf512b681c48e3d62827e736429
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-13 22:06:24 +0100

    Implement light and dark themes in AppTheme
    Added light and dark theme configurations using Material 3 and Google Fonts.

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xFF6366F1),
+        brightness: Brightness.light,
+        surface: const Color(0xFFF8FAFC),
+      ),
+      textTheme: GoogleFonts.plusJakartaSansTextTheme(ThemeData.light().textTheme),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xFF818CF8),
+        brightness: Brightness.dark,
+        surface: const Color(0xFF0F172A),
+      ),
+      textTheme: GoogleFonts.plusJakartaSansTextTheme(ThemeData.dark().textTheme),
+    );
+  }
+}
